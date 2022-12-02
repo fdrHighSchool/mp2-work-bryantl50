0e64f07c62ae0df7c2d61780bac6f8f42a83c2f4
Bryant Lin <114942637+bryantl50@users.noreply.github.com>
2022-12-02 05:21:06 -0500

    Create Shapes.java

---
+++ 06-shapes/Shapes.java
@@ -0,0 +1,50 @@
+class Shapes {
+    public static void main(String[] args) {
+        System.out.println("Shapes");
+        square(5);
+        rectangle(8, 10);
+        triangle(5);
+    }
+    //end main
+    public static void square(int x) {
+        for(int row = 1; row <= x; row++) {
+            System.out.println("");
+            for (int col = 1; col <= x; col++) {
+                System.out.print("* ");
+            }
+            //adds the amount of stars to each row
+        }
+        //creates rows
+    }
+    //end square
+    public static void rectangle(int x, int y) {
+        for(int row = 1; row <= x; row++) {
+            System.out.println("");
+            for (int col = 1; col <= y; col++) {
+                System.out.print("* ");
+            }
+            //adds the amount of stars to each row
+        }
+        //creates rows
+    }
+    //end rectangle
+    public static void triangle(int x) {
+        for(int row = 1; row <= x; row++) {
+            System.out.println("");
+            for (int space = 1; space <= x-row; space++) {
+                System.out.print(" ");
+            }
+            //adds spaces, same # as user input - row #
+            for (int col = 1; col <= row; col++) {
+                System.out.print("* ");
+            }
+            //adds the amount of stars to each row
+        }
+        //adds rows
+    }
+    //end triangle
+}
+//end class
+
+// rectangle, two outputs 
+// triangle, one ouptut 
